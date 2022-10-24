7aaa167d60b079063569d924d6af11558a7b957c
Jessica Cestariolli <jcestariolli@users.noreply.github.com>
2022-10-24 14:27:20 -0300

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import challenges.thitydaysofcode.Day10;
+import interfaces.Challenge;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] argh) throws IOException {
+        Challenge challengeToExecute = new Day10();
+        challengeToExecute.execute();
+    }
+}
