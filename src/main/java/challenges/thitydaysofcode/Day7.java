19d9c73bd18c5641b0f808dd413e00f8ff244ef3
Jessica Cestariolli <jcestariolli@users.noreply.github.com>
2022-10-24 14:52:57 -0300

    Update Day7.java

--- src/main/java/challenges/thitydaysofcode/Day7.java
+++ src/main/java/challenges/thitydaysofcode/Day7.java
@@ -2,12 +2,28 @@ package challenges.thitydaysofcode;
 
 import interfaces.Challenge;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Day7 implements Challenge {
 
+
     @Override
     public void execute() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" ")).map(Integer::parseInt).collect(toList());
+        String output = IntStream.range(0, n).mapToObj(i -> arr.get(n - 1 - i)).map(i -> String.valueOf(i)).collect(Collectors.joining(" "));
+        System.out.println(output);
+        bufferedReader.close();
     }
 }
