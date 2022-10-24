a48291689712e51532a11736fcd1b262155b07a9
J Cestariolli <jessica.cestariolli@gmail.com>
2022-10-24 19:26:08 -0300

    upd Main.java add comment about where to exchange the challenge class name to execute

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] argh) throws IOException {
+
+        // TODO instantiate the challenge to execute it here
         Challenge challengeToExecute = new Day10();
+
         challengeToExecute.execute();
     }
 }
