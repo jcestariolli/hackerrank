6a7fa46306add690f787ba1c4749adf7c1b761e5
Jessica Cestariolli <jcestariolli@users.noreply.github.com>
2022-10-24 14:51:48 -0300

    Update Day3.java

--- src/main/java/challenges/thitydaysofcode/Day3.java
+++ src/main/java/challenges/thitydaysofcode/Day3.java
@@ -2,12 +2,26 @@ package challenges.thitydaysofcode;
 
 import interfaces.Challenge;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Day3 implements Challenge {
 
+
     @Override
     public void execute() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        if (n % 2 != 0) {
+            System.out.println("Weird");
+        } else if (n > 5 && n < 21) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+        bufferedReader.close();
     }
 }
