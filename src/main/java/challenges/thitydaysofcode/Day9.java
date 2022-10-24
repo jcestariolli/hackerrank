5021118ce917f93be8a696089077bac3863ee961
Jessica Cestariolli <jcestariolli@users.noreply.github.com>
2022-10-24 14:25:21 -0300

    Create Day9.java

---
+++ src/main/java/challenges/thitydaysofcode/Day9.java
@@ -0,0 +1,39 @@
+package challenges.thitydaysofcode;
+
+import interfaces.Challenge;
+
+import java.io.*;
+
+public class Day9 implements Challenge {
+    @Override
+    public void execute() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.factorial(n);
+
+        System.out.println(result);
+        //bufferedWriter.write(String.valueOf(result));
+        //bufferedWriter.newLine();
+
+        bufferedReader.close();
+        //bufferedWriter.close();
+
+    }
+}
+
+class Result {
+
+    /*
+     * Complete the 'factorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int factorial(int n) {
+        return (n==1) ? 1 : n * factorial(n-1);
+    }
+}
