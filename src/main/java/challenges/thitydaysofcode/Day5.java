292c3a65f135e5553978c209d9417cbcf7e77f26
Jessica Cestariolli <jcestariolli@users.noreply.github.com>
2022-10-24 14:33:02 -0300

    Create Day5.java

---
+++ src/main/java/challenges/thitydaysofcode/Day5.java
@@ -0,0 +1,13 @@
+package challenges.thitydaysofcode;
+
+import interfaces.Challenge;
+
+import java.io.IOException;
+
+public class Day5 implements Challenge {
+
+    @Override
+    public void execute() throws IOException {
+
+    }
+}
