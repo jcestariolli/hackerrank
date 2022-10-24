573d7c5047071e13549dc353fa719e318503c12d
Jessica Cestariolli <jcestariolli@users.noreply.github.com>
2022-10-24 14:31:32 -0300

    Create Day2.java

---
+++ src/main/java/challenges/thitydaysofcode/Day2.java
@@ -0,0 +1,13 @@
+package challenges.thitydaysofcode;
+
+import interfaces.Challenge;
+
+import java.io.IOException;
+
+public class Day2 implements Challenge {
+
+    @Override
+    public void execute() throws IOException {
+
+    }
+}
