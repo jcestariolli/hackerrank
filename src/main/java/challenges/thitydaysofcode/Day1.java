1bf71e00cb7eb964a58dda7819d5244384b06404
Jessica Cestariolli <jcestariolli@users.noreply.github.com>
2022-10-24 14:51:05 -0300

    Update Day1.java

--- src/main/java/challenges/thitydaysofcode/Day1.java
+++ src/main/java/challenges/thitydaysofcode/Day1.java
@@ -3,11 +3,27 @@ package challenges.thitydaysofcode;
 import interfaces.Challenge;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Day1 implements Challenge {
 
     @Override
     public void execute() throws IOException {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
 
+        Scanner scan = new Scanner(System.in);
+
+        int intInput = scan.nextInt();
+        scan.nextLine();
+        double doubleInput = scan.nextDouble();
+        scan.nextLine();
+        String stringInput = scan.nextLine();
+
+        System.out.println(i + intInput);
+        System.out.println(new DecimalFormat("#.0").format(d + doubleInput));
+        System.out.println(s + stringInput);
     }
 }
