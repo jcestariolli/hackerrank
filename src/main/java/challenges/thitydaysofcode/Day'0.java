36be2c8a705d4511b92eb413973e9bbe69a14d3a
Jessica Cestariolli <jcestariolli@users.noreply.github.com>
2022-10-24 14:25:46 -0300

    Create Day'0.java

---
+++ src/main/java/challenges/thitydaysofcode/Day'0.java
@@ -0,0 +1,34 @@
+package challenges.thitydaysofcode;
+
+import interfaces.Challenge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day10 implements Challenge {
+    @Override
+    public void execute() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int majorConsecutive1Counter = 0;
+        int consecutive1Counter = 0;
+        for (; n > 0; n = n / 2) {
+            int binaryBase = n % 2;
+            if (binaryBase == 1) {
+                consecutive1Counter++;
+                if (consecutive1Counter > majorConsecutive1Counter) {
+                    majorConsecutive1Counter = consecutive1Counter;
+                }
+            } else {
+                consecutive1Counter = 0;
+            }
+
+        }
+        System.out.println(majorConsecutive1Counter);
+
+        bufferedReader.close();
+    }
+}
