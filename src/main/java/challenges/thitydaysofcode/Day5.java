abce9afb63c97411be5516c8b7a62a3abb04c5ea
Jessica Cestariolli <jcestariolli@users.noreply.github.com>
2022-10-24 14:52:29 -0300

    Update Day5.java

--- src/main/java/challenges/thitydaysofcode/Day5.java
+++ src/main/java/challenges/thitydaysofcode/Day5.java
@@ -2,12 +2,23 @@ package challenges.thitydaysofcode;
 
 import interfaces.Challenge;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Day5 implements Challenge {
 
+
     @Override
     public void execute() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + n * i);
+        }
+        bufferedReader.close();
     }
+
+
 }
