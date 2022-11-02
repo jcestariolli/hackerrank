9b6c4e5d4bfd46fa18be1b1428b56a0311a2ee72
J Cestariolli <jessica.cestariolli@gmail.com>
2022-11-02 10:55:41 -0300

    add Day17.java

---
+++ src/main/java/challenges/thirtydaysofcode/Day17.java
@@ -0,0 +1,43 @@
+package challenges.thirtydaysofcode;
+
+import interfaces.Challenge;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Day17 implements Challenge {
+
+    @Override
+    public void execute() throws IOException {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
+
+class Calculator {
+
+    public int power(int n, int p) throws Exception {
+        if (n < 0 || p < 0) throw  new Exception("n and p should be non-negative");
+        if (p == 0) return 1;
+        if (n == 0) return 0;
+        int result = n;
+        for (int i = 1; i < p ; i++) {
+            result*=n;
+        }
+        return result;
+    }
+}
