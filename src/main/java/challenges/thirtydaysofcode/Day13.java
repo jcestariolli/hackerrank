e6453e1ca92eb436312126cdd38a482e26e6aabe
J Cestariolli <jessica.cestariolli@gmail.com>
2022-11-02 10:04:39 -0300

    add Day13.java

---
+++ src/main/java/challenges/thirtydaysofcode/Day13.java
@@ -0,0 +1,52 @@
+package challenges.thirtydaysofcode;
+
+import interfaces.Challenge;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Day13 implements Challenge {
+
+    @Override
+    public void execute() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
+
+abstract class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+
+// Declare your class here. Do not use the 'public' access modifier.
+// Declare the price instance variable
+class MyBook extends Book {
+
+    protected int price;
+
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    @Override
+    void display() {
+        System.out.println("Title: " + this.title);
+        System.out.println("Author: " + this.author);
+        System.out.println("Price: " + this.price);
+    }
+}
